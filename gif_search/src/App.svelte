0ae9d29b45b3b4915ee04756317a475d6993eaef
Michael Houmann <mhoumann87@gmail.com>
2019-08-26 10:40:24 +0200

    initial commit

---
+++ gif_search/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import config from "./config.js";
+
+  console.log(config.apiKey);
+  export let search = "";
+</script>
+
+<style>
+
+</style>
+
+<form>
+  <label for="search">Search</label>
+  <input bind:value={search} id="search" name="search" />
+</form>
+<h2>{search}</h2>
